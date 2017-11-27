3b3c7eef2fafb12194c958197603257a30fb4449
Ben Kaiser <bkaiser94@gmail.com>
2017-11-27 17:58:31 -0500

    changed config file to include different wavelength ranges to reflect JJ's recommendations and my own suspicions about useful ranges to have

--- run_step_grid.py
+++ run_step_grid.py
@@ -14,8 +14,11 @@ calls try_lightcurve.py and then runs it over a grid of step sizes, and potentia
 
 target_dir = sys.argv[1] + '/'
 #step_grid= np.arange(1,121, 1) #by going one above the desired value, it is included in the range. For example to include 120 make it 121.
-step_grid= np.arange(15,121, 15) #by going one above the desired value, it is included in the range. For example to include 120 make it 121.
+#step_grid= np.arange(15,121, 15) #by going one above the desired value, it is included in the range. For example to include 120 make it 121.
 #limit_list= [[1130,1430],[1130,1800],[1130,1850],[1130,1900]]
+step_grid= np.arange(5,31,5)
+app_array= np.arange(45,151,15)
+step_grid= np.append(step_grid, app_array)
 
 def run_step_grid(target_dir):
     for wave_limit in config.wave_limit_list:
