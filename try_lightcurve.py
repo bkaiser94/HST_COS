8ac9b899d1f5e77f22dfea72917cf21d9d8127f4
Ben Kaiser <bkaiser94@gmail.com>
2017-12-08 16:46:38 -0500

    commented airglow mask print statements in cos.py. changed the method of correcting the time values to do the actual correction for the barycentric value using astropy.time.Time.light_travel_time(), and fed it the inputs to assume the observer was located at 0 degrees latitude and longitude on the surface of the earth, which is slightly incorrect as HST is in orbit (so there's a velocity correction for the TDB in addition to its rapidly varying position around the Earth) and is located somewhat higher. Eastman et al 2010 cites a 21.3 ms amplitude variation with a period of 1 sidereal day for neglecting the light travel time difference between the center of the Earth and the surface, but we're still a bit different from that.

--- try_lightcurve.py
+++ try_lightcurve.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import sys
 from astropy.time import Time
-
+import astropy.coordinates as coord
+import astropy.units as u
 from calcos import calcos
 from costools import timefilter
 #import lightcurve as lc
@@ -16,7 +17,8 @@ import local_lightcurve as lc #I have created a second directory within here tha
 #target_dir = sys.argv[1] + '/'
 #stepsize = int(sys.argv[2]) 
 second_per_mjd= 1./1.15741e-5     #SECOND_PER_MJD value from lightcurve.cos.extract, but I couldn't import it for whatever reason... so I just copied and pasted
-
+start_dir = os.getcwd()
+print "start_dir: ", start_dir
 def make_lightcurve(target_dir, stepsize, wlim, plotall=True):
     """
     """
@@ -26,7 +28,7 @@ def make_lightcurve(target_dir, stepsize, wlim, plotall=True):
     print "plotall=", plotall
     print "target_dir: ", target_dir
     #wlim= [1130, 1900] #3200 is the default max from what I could gather as is 915. The geocoronal emission lines are automatically excluded from the lightcurve
-    band= '_a' #should be '_a', '_b', '' for the combined band images, or  '*' to get all bands available (the '*' option actually causes cos.py to double count bands)
+    band= '_a' #should be '_a', '_b', '' for the combined band images, or  '*' to get all bands available (the '*' option actually causes cos.py to double-count bands)
 
     mjd_array = np.array([])
 
@@ -42,7 +44,8 @@ def make_lightcurve(target_dir, stepsize, wlim, plotall=True):
 
 
     print os.getcwd()
-    for item in glob(target_dir + '*corrtag' + band + '.fits'):
+    assembled_files= glob(target_dir + '*corrtag' + band + '.fits')
+    for item in assembled_files:
         astrotable, astrometa = lc.cos.extract(item, step=stepsize, wlim= wlim)
         #print 'astrometa: ', astrometa
         
@@ -74,14 +77,14 @@ def make_lightcurve(target_dir, stepsize, wlim, plotall=True):
     #flux_array = np.copy( flux_table/np.nanmean(flux_table)-1.) #normalize flux array around zero #This was the mean all norming method
     #flux_array = np.copy(flux_table) #this should be uncommented for the individual fits normalization
     flux_array= np.copy(flux_table/np.nanmedian(flux_table)-1.) #New version as of 2017-11-30
-    print "np.nanmean(flux_array)" , np.nanmean(flux_array)
-    print "np.nanmean(flux_table: " ,np.nanmean(flux_table)
-    print "np.sum(flux_array-pre_flux): ", np.sum(flux_array- pre_flux)
-    print "np.sum(flux_array + pre_flux): ", np.sum(flux_array + pre_flux)
-    print ""
-    print "np.nanmax(flux_array): ", np.nanmax(flux_array)
-    print "mjd value for that: ", mjd_array[np.argmax(flux_array)]
-    print "top 5 flux values: ", np.sort(flux_array)[-5:]
+    #print "np.nanmean(flux_array)" , np.nanmean(flux_array)
+    #print "np.nanmean(flux_table: " ,np.nanmean(flux_table)
+    #print "np.sum(flux_array-pre_flux): ", np.sum(flux_array- pre_flux)
+    #print "np.sum(flux_array + pre_flux): ", np.sum(flux_array + pre_flux)
+    #print ""
+    #print "np.nanmax(flux_array): ", np.nanmax(flux_array)
+    #print "mjd value for that: ", mjd_array[np.argmax(flux_array)]
+    #print "top 5 flux values: ", np.sort(flux_array)[-5:]
 
     #print 'mjd_array.shape: ', mjd_array.shape
     #print 'gross_array.shape: ', gross_array.shape
@@ -125,18 +128,31 @@ def make_lightcurve(target_dir, stepsize, wlim, plotall=True):
     else:
         pass
     #see if this is being run exernally to see if we should be gathering the lightcurves somewhere
+    
+    #time_sec= (mjd_array- mjd_array[0])*second_per_mjd #This needs to be changed for the times in seconds to be in terms of bmjd_tdb
+    sub_dir,sub_file = assembled_files[0].split('/')
+    
+    os.chdir(sub_dir+'/') #change into the directory that contains the files so we can look at the header
+    hdu= fits.open(sub_file) #look at the header
+    os.chdir('../') #change back to the original directory we were in
+    target_ra= hdu[0].header['RA_TARG'] #header in degrees
+    target_dec= hdu[0].header['DEC_TARG'] #headers in degrees
+    print "starting time corrections"
+    target_coord = coord.SkyCoord(target_ra, target_dec, unit= (u.deg, u.deg), frame= 'icrs')
+    time_mjd= Time(mjd_array, format= 'mjd', scale= 'utc', location= (0.*u.deg, 0.* u.deg)) #assumes location at lon = 0 , lat =0, elev= sea level
+    ltt_bary= time_mjd.light_travel_time(target_coord) #light travel time for the target for the barycentric correction
+    print "light travel time calculated"
+    print "max time correction: ", np.max(np.abs(ltt_bary))
+    bmjd_array= (time_mjd.tdb + ltt_bary).tdb.mjd #barycentric correction
+    time_sec= (bmjd_array- bmjd_array[0])*second_per_mjd #This should correctly output the seconds times in the BMJD_tdb version
+    textheader= 'mjd\tgross\tflux\ttime(s)(bmjd_tdb)\tbmjd_tdb'
+    #textcomment= 'step = ' + str(stepsize)
     if __name__ != "__main__":
         dest_dir = target_dir[:-1] + "_grid_lightcurves/"
         if not os.path.exists(dest_dir):
             os.makedirs(dest_dir)
         os.chdir(dest_dir)
     print os.getcwd()
-    #time_sec= (mjd_array- mjd_array[0])*second_per_mjd #This needs to be changed for the times in seconds to be in terms of bmjd_tdb
-    time_mjd= Time(mjd_array, format= 'mjd', scale= 'utc')
-    bmjd_array= time_mjd.tdb.mjd
-    time_sec= (bmjd_array- bmjd_array[0])*second_per_mjd #This should correctly output the seconds times in the BMJD_tdb version
-    textheader= 'mjd\tgross\tflux\ttime(s)(bmjd_tdb)\tbmjd_tdb'
-    #textcomment= 'step = ' + str(stepsize)
     out_array= np.append([mjd_array], [gross_array], axis = 0)
     out_array = np.append(out_array, [flux_array], axis = 0)
     out_array = np.append(out_array, [time_sec], axis=0)
