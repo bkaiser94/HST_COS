bfe0e022a9512a89e41ebe2cb3578da49678e992
Ben Kaiser <bkaiser94@gmail.com>
2017-12-11 15:54:53 -0500

    created setup-dependent wavelength limit selection to decrease memory and storage demands as well as reducing runtime by eliminating unnecessary wavelength bins for different setups.

--- run_step_grid.py
+++ run_step_grid.py
@@ -3,7 +3,7 @@ from glob import glob
 import os
 import sys
 import config
-
+from astropy.io import fits
 import try_lightcurve as tlc
 
 
@@ -20,9 +20,32 @@ step_grid= np.arange(5,31,5)
 app_array= np.arange(45,61,15)
 step_grid= np.append(step_grid, app_array)
 step_grid= np.append(step_grid, [90,120,180,240,300])
+def get_wave_limits(target_dir):
+    #open a fits file and get the central wavelength and grating headers
+    #look through the different if statements to find the appropriate grating
+    #then look in the if-statements under that category for the correct wave_limit_list
+    #This could probably be rewritten to branch through a dictionary of dictionaries of lists... but whatever
+    checked_file = glob(target_dir+ '*corrtag*.fits')[0]
+    hdu = fits.open(checked_file)
+    grating= hdu[0].header['OPT_ELEM']
+    center_wave= hdu[0].header['CENWAVE']
+    if grating == "G130M":
+        if center_wave == 1291:
+            list_limits= config.G130M_1291A_wave_list
+        elif center_wave == 1300:
+            list_limits = config.G130M_1300A_wave_list
+    elif grating == "G140L":
+        if center_wave == 1105:
+            list_limits = config.G140L_1105A_wave_list
+    elif grating == "G160M":
+        if center_wave == 1600:
+            list_limits = config.G160M_1600A_wave_list
+    return list_limits
+
 
 def run_step_grid(target_dir):
-    for wave_limit in config.wave_limit_list:
+    list_limits = get_wave_limits(target_dir)
+    for wave_limit in list_limits:
         try:
             outstring = tlc.make_lightcurve(target_dir, 1, wave_limit, plotall = False) #one second binning before running the other steps in the grid
         except IndexError as error:
@@ -37,6 +60,7 @@ def run_step_grid(target_dir):
                 print error
                 print "******************************\n"
     return ''
+
 
 if __name__ == '__main__':
     run_step_grid(target_dir)
