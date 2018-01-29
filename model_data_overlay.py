c1379738d578dd31bb257e652ccf3ddf10e02461
Ben Kaiser <bkaiser94@gmail.com>
2018-01-28 23:37:51 -0500

    created model_data_overlay.py to plot the spectra from a target with the model that most closely matches it.

---
+++ model_data_overlay.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import config
+import os
+from glob import glob
+
+
+
+#path to the model files
+teff= 12000
+logg= 8.0 #model values to pull (should be changed to be read from somewhere in the future)
+#
