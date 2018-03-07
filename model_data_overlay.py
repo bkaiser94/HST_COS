f4f54047665908c2ba63e252297b5c63b4073045
Ben Kaiser <bkaiser94@gmail.com>
2018-03-07 09:56:36 -0500

    model fitting by iterating through the grid now. There is also a normalization by the model values

--- model_data_overlay.py
+++ model_data_overlay.py
@@ -6,6 +6,7 @@ import os
 from glob import glob
 from astropy.io import fits
 
+import config
 import spec_plot_tools as spt
 
 """
@@ -28,6 +29,7 @@ scale_wave_range= [1400, 1420]
 target_x1ds= glob(target_path+'*x1dsum.fits')
 target_file= target_x1ds[0] #I only want the first one for now; when I eventually make this include all the visits for a given target I'll introduce some sort of function for combining them or whatever
 model_path= '/Users/BenKaiser/Desktop/DK_2010/'
+model_extension = '.dat'
 #scaling_coefficient= 5.2e21
 #wavelength range to consider for the scaling
 
@@ -43,6 +45,7 @@ print target_spec[0]
 #function to trim the model plot to limit it to the range of the target's spectrum
 #At some point I'll have to implement masking
 
+
 def get_model_spec(fitted_teff, fitted_logg):
     """
     inputs: fitted_teff, fitted_logg
@@ -51,14 +54,23 @@ def get_model_spec(fitted_teff, fitted_logg):
     """
     padded_teff = fitted_teff #needs to be changed to pad/remove decimals (and convert to string)
     padded_logg= fitted_logg #needs to be changed to pad/remove decimals (and convert to string)
-    model_file= 'da' +padded_teff + '_' + padded_logg + '.dat'
+    model_file= 'da' +padded_teff + '_' + padded_logg + model_extension
     model_file= model_path + model_file
     model_file= glob(model_file)[0]
     model_spec= np.genfromtxt(model_file)
     model_spec= model_spec.T #transpose because numpy is weird on the axes it reads in.
-    print "model_spec.shape:",  model_spec.shape
+    #print "model_spec.shape:",  model_spec.shape
     return model_spec
 
+def get_model_fromfile(model_file):
+    """
+    inputs: model file name
+    
+    output: 2-d numpy array containing the wavelengths and spectral values from the model grid for the input teff and logg
+    """
+    model_spec= np.genfromtxt(model_file)
+    model_spec= model_spec.T #transpose because numpy is weird on the axes it reads in.
+    return model_spec
 
 #########
 
@@ -69,7 +81,19 @@ def get_scale_factor(target_spec, model_spec, wave_range):
     model_mean = np.mean(model_spec[1][allowed_model])
     scaling_factor =target_mean/model_mean
     return scaling_factor
+####
 
+def calc_sq_dist(target_spec, model_spec):
+    interp_model_flux = np.interp(target_spec[0], model_spec[0], model_spec[1])
+    interp_model= np.vstack([np.copy(target_spec[0]),interp_model_flux])
+    #print "interp_model.shape", interp_model.shape
+    norm_difs = np.abs(interp_model[1]-target_spec[1])/np.float_(target_spec[1])
+    nan_remove = np.isinf(norm_difs)
+    norm_difs= norm_difs[~nan_remove]
+    dif = np.sum(norm_difs)/norm_difs.shape[0]
+    
+    return dif
+    
 ###
 def plot_overlays(spec1, spec2, scaling_coefficient):
     plt.plot(spec1[0], spec1[1], label = 'observed')
@@ -77,21 +101,41 @@ def plot_overlays(spec1, spec2, scaling_coefficient):
     plt.legend(numpoints=1, fontsize=14, loc='best' )
     plt.xlabel(r'Wavelength ($\AA$)')
     plt.ylabel('Flux (cgs units)')
-    plt.title(target_path[:-1] +'_' +  teff + '_' + logg)
+    plt.title(target_path[:-1] )
     plt.show()
     return ''
 
 
-##########
-print os.getcwd()
+        
 
-model_spec= get_model_spec(teff, logg)
+
+##########
+#model_spec= get_model_spec(teff, logg)
 #target_spec= remove_range(target_spec[0], target_spec[1], lyman_alpha)
 #target_spec= remove_range(target_spec[0], target_spec[1], oxygen)
 #target_spec= remove_range(target_spec[0], target_spec[1], seg_gap)
 #target_spec = sort_spectrum(target_spec)
-mask_list= [lyman_alpha]+[oxygen]+[seg_gap]+[nitrogen]
-target_spec = spt.clean_spectrum(target_spec, min_wave, max_wave, mask_list)
-model_spec= spt.trim_spec(model_spec, np.min(target_spec[0]), np.max(target_spec[0]))
-scaling_coefficient= get_scale_factor(target_spec, model_spec, scale_wave_range)
-plot_overlays(target_spec, model_spec, scaling_coefficient)
+print os.getcwd()
+def run_model_grid(target_spec):
+    mask_list= [lyman_alpha]+[oxygen]+[seg_gap]+[nitrogen]
+    target_spec = spt.clean_spectrum(target_spec, min_wave, max_wave, mask_list)
+    model_file_list = glob(model_path+'*'+model_extension)
+    dist_list = []
+    for model_file in model_file_list:
+        model_spec = get_model_fromfile(model_file)
+        model_spec= spt.trim_spec(model_spec, np.min(target_spec[0]), np.max(target_spec[0]))
+        scaling_coefficient= get_scale_factor(target_spec, model_spec, scale_wave_range)
+        new_dist = calc_sq_dist(target_spec, model_spec)
+        dist_list.append(new_dist)
+       
+    dist_array = np.array(dist_list)
+    for mod_file, dist_mod in zip(model_file_list, dist_list):
+        print "model_file:", mod_file, "difference:", dist_mod
+    min_index = np.argmin(dist_list)
+    min_model = model_file_list[min_index]
+    min_dist = dist_array[min_index]
+    print "best fit model:", min_model
+    
+    plot_overlays(target_spec, model_spec, scaling_coefficient)
+    
+run_model_grid(target_spec)
