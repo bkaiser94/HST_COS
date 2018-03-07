0ced79d94860f271546f951fae895dc9fe6826b9
Ben Kaiser <bkaiser94@gmail.com>
2018-03-07 10:26:56 -0500

    normalization by model and segment gap mask and the geocoronal line masks are all called from config.py (at least for model_data_overlay.py)

--- model_data_overlay.py
+++ model_data_overlay.py
@@ -18,13 +18,19 @@ target_path = sys.argv[1]+'/' #the input should be the target name just like all
 teff= "013500" #hard coded for test target
 #logg= 8.0 #model values to pull (should be changed to be read from somewhere in the future)
 logg = "800" #hard coded for test target(it was 7.8 in the paper, but that is technically the same as this gridpoint to two sig figs)
-min_wave= 1142
-max_wave= 1422
-lyman_alpha = [1214,1217] #probably should put this into the config file as the masking for the spectroscopic fitting (but really this goes back to the need for individual text files by target for the various maskings that are required)
-oxygen= [1300, 1308]
-nitrogen=[1199,1201]
-seg_gap = [1268,1298] #segment gap mask used for photometry for the G130M at 1291 Angs
-scale_wave_range= [1400, 1420] 
+#min_wave= 1142
+#max_wave= 1422
+min_wave= 1180
+max_wave= 1800
+#lyman_alpha = [1214,1217] #probably should put this into the config file as the masking for the spectroscopic fitting (but really this goes back to the need for individual text files by target for the various maskings that are required)
+#oxygen= [1300, 1308]
+#nitrogen=[1199,1201]
+lyman_alpha = config.lyman_mask
+oxygen= config.oxygen_mask
+nitrogen = config.nitrogen_mask
+#seg_gap = [1268,1298] #segment gap mask used for photometry for the G130M at 1291 Angs
+#scale_wave_range= [1400, 1420] 
+scale_wave_range= [1450,1550] 
 #scale factor to be just typed in for the moment but should be replaced by a function at some point that calculates it for you
 target_x1ds= glob(target_path+'*x1dsum.fits')
 target_file= target_x1ds[0] #I only want the first one for now; when I eventually make this include all the visits for a given target I'll introduce some sort of function for combining them or whatever
@@ -34,12 +40,15 @@ model_extension = '.dat'
 #wavelength range to consider for the scaling
 
 target_hdu= fits.open(target_file)
-print "Centwave: ", target_hdu[0].header['CENWAVE']
+cenwave=target_hdu[0].header['CENWAVE']
+grating = target_hdu[0].header['OPT_ELEM']
+seg_gap = config.seg_gap_dict[str(grating)][str(cenwave)]
+print "grating (OPT_ELEM):", grating, "Centwave: ", target_hdu[0].header['CENWAVE']
 target_waves = np.copy(target_hdu[1].data['wavelength'].ravel())
 target_flux= np.copy(target_hdu[1].data['flux'].ravel())
 target_spec= np.vstack([target_waves, target_flux])
-print target_spec.shape
-print target_spec[0]
+#print target_spec.shape
+#print target_spec[0]
 
 
 #function to trim the model plot to limit it to the range of the target's spectrum
@@ -95,9 +104,9 @@ def calc_sq_dist(target_spec, model_spec):
     return dif
     
 ###
-def plot_overlays(spec1, spec2, scaling_coefficient):
+def plot_overlays(spec1, spec2):
     plt.plot(spec1[0], spec1[1], label = 'observed')
-    plt.plot(spec2[0], spec2[1]*scaling_coefficient, label= 'model', color = 'r')
+    plt.plot(spec2[0], spec2[1], label= 'model', color = 'r')
     plt.legend(numpoints=1, fontsize=14, loc='best' )
     plt.xlabel(r'Wavelength ($\AA$)')
     plt.ylabel('Flux (cgs units)')
@@ -125,6 +134,7 @@ def run_model_grid(target_spec):
         model_spec = get_model_fromfile(model_file)
         model_spec= spt.trim_spec(model_spec, np.min(target_spec[0]), np.max(target_spec[0]))
         scaling_coefficient= get_scale_factor(target_spec, model_spec, scale_wave_range)
+        model_spec[1]=model_spec[1]*scaling_coefficient
         new_dist = calc_sq_dist(target_spec, model_spec)
         dist_list.append(new_dist)
        
@@ -135,7 +145,11 @@ def run_model_grid(target_spec):
     min_model = model_file_list[min_index]
     min_dist = dist_array[min_index]
     print "best fit model:", min_model
+    model_spec= get_model_fromfile(min_model)
+    model_spec = spt.trim_spec(model_spec, np.min(target_spec[0]), np.max(target_spec[0]))
+    scaling_coefficient= get_scale_factor(target_spec, model_spec, scale_wave_range)
+    model_spec[1]= model_spec[1]*scaling_coefficient
     
-    plot_overlays(target_spec, model_spec, scaling_coefficient)
+    plot_overlays(target_spec, model_spec)
     
 run_model_grid(target_spec)
