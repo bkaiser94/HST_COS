af16aef171b897fb736486165bf280f2f087e7b7
Ben Kaiser <bkaiser94@gmail.com>
2018-06-05 11:54:04 -0400

    added in-phase binning of flux for another output. It technically just places the timestamp in the middle of the bin however, instead of flux-weighting its location in time as recommended by Eastman et al. 2010

--- period_folding.py
+++ period_folding.py
@@ -18,6 +18,7 @@ inputfile= sys.argv[1]
 period= np.float_(sys.argv[2])
 unit_arg = sys.argv[3]
 second_per_mjd= 1./1.15741e-5     #SECOND_PER_MJD value from lightcurve.cos.extract, but I couldn't import it for whatever reason... so I just copied and pasted
+phase_bins = 120. #number of bins to do with in-phase binning
 
 if unit_arg.startswith('s'):
     print "period in seconds"
@@ -39,7 +40,28 @@ elif unit_arg.startswith('m'):
     time_converter = second_per_mjd/60.
     time_string = 'min'
     
-
+def bin_in_phase(fold_times, flux):
+    #sorted_indices = np.argsort(fold_times)
+    #sorted_fold_times = fold_times[sorted_indices]
+    #sorted_flux = flux[sorted_indices]
+    bin_width = period/phase_bins
+    bin_edges = np.linspace(0,period+bin_width, phase_bins)
+    binned_times = []
+    binned_flux = []
+    binned_std = []
+    for i in range(0,int(phase_bins)-1):
+        condition  = (fold_times >= bin_edges[i]).astype(int) * (fold_times <= bin_edges[i+1]).astype(int) #array of zeros and ones for values that are in bounds
+        good_vals = np.where(condition> 0)
+        good_flux = flux[good_vals]
+        print good_flux.shape[0]
+        std_err = np.std(good_flux)
+        bin_flux = np.mean(good_flux)
+        binned_flux.append(bin_flux)
+        binned_std.append(std_err)
+        binned_times.append(np.mean([bin_edges[i], bin_edges[i+1]]))
+    return np.array(binned_times), np.array(binned_flux), np.array(binned_std)
+        
+    
 all_array = np.genfromtxt(inputfile, names=True)
 #times= Time(all_array['mjd'], format='mjd')
 #times= np.copy(all_array['mjd'])
@@ -50,12 +72,29 @@ times= (times - times[0]) *time_converter
 #fold_times = times%period
 fold_times= np.mod(times, period)
 
+print "Base time unit:", times[1]-times[0], times[2]-times[1], times[3]-times[2]
+
 
 plt.figure(figsize= (20,9))
 #plt.axhline(y= 1, linestyle= '-', color = 'm', xmin = 0, xmax = 100000, linewidth = 1, alpha = 0.2)
 plt.axhline(y= 0,linestyle = '-', color = 'g' , xmin = 0, xmax = 100000, linewidth = 1, alpha = 0.2)
 plt.scatter(fold_times, fluxes)
 plt.xlabel("Time ("+ time_string + ")")
+plt.xlim(0, period)
+plt.title(inputfile + ' Period fold'+ str(period) + ' ' + time_string)
+plt.show()
+
+binned_times, binned_flux, binned_std = bin_in_phase(fold_times, fluxes)
+print binned_std
+plt.figure(figsize= (20,9))
+#plt.axhline(y= 1, linestyle= '-', color = 'm', xmin = 0, xmax = 100000, linewidth = 1, alpha = 0.2)
+plt.axhline(y= 0,linestyle = '-', color = 'g' , xmin = 0, xmax = 100000, linewidth = 1, alpha = 0.2)
+#plt.scatter(binned_times, binned_flux)
+#plt.plot(binned_times, binned_flux, marker = 'o')
+plt.errorbar(binned_times, binned_flux, yerr= binned_std,  marker = 'o')
+
+plt.xlabel("Time ("+ time_string + ")")
+plt.ylabel("Mean normalized flux")
 plt.xlim(0, period)
 plt.title(inputfile + ' Period fold'+ str(period) + ' ' + time_string)
 plt.show()
