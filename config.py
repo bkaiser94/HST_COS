47cdda6924f49d4fd088ac912a09e187d6d22759
Ben Kaiser <bkaiser94@gmail.com>
2018-06-05 16:35:04 -0400

    Now with support for G130M at 1309A and 1222A! (1222A was done without an FP-POS=4 spectrum available so it's not as reliable).

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ local_lightcurve.cos.py does actually look here now for the masking values, so n
 """
 #from astropy.time import Time
 #booleans for whether or not to actually do the masking of the different lines
-do_lyman = True #this one just does a much smaller mask when disabled. (It would be the only thing plotted otherwise)
+do_lyman = False #this one just does a much smaller mask when disabled. (It would be the only thing plotted otherwise)
 do_oxygen= True #smaller mask when False (good for G130M setup)
 do_nitrogen= True
 cos_refresh_rate = 0.032 #seconds
@@ -18,13 +18,15 @@ gain_change_list= ['2009-05-11', '2009-08-12', '2011-03-08', '2012-03-26', '2012
 #wave_limit_list =  [[1180,1300],[1300,1500],[1500,1700],[1180,1800],[1250,1271],[1150,1200],[1300,1425],[1150,1425],[1150,1271],[1452, 1570],[1615,1760]] #new ones as of 2017-11-28
 G160M_1600A_wave_list= [[1452, 1570],[1615,1760],[1452,1760]]
 G160M_1577A_wave_list= [[1387,1557],[1578,1742],[1387,1742]] #Based only on the GW-Librae observation which only included 4 exposures all in FP-POS 3
+G130M_1222A_wave_list = [[1077,1201],[1233,1354],[1077,1354],[1250,1271]]
 G130M_1291A_wave_list=[[1142, 1268],[1298,1422],[1142,1422], [1142,1200],[1250,1268]]
 #G130M_1300A_wave_list= G130M_1291A_wave_list #these are the same for now
 G130M_1300A_wave_list= [[1250,1271],[1150,1200],[1300,1425],[1150,1425],[1150,1271]]
+G130M_1309A_wave_list = [[1162,1286],[1318,1442],[1162,1442],[1250,1271], [1162,1200]]
 G130M_1327A_wave_list= [[1182,1307],[1335,1460],[1182,1460]] 
 G140L_1105A_wave_list= [[1180,1300],[1300,1500],[1500,1700],[1180,1800]]
 G130M_1096A_wave_list= [[955, 1075],[1106,1232], [955,1232]]
-wave_limit_list= G160M_1600A_wave_list+G130M_1291A_wave_list+ G130M_1300A_wave_list + G140L_1105A_wave_list+G130M_1096A_wave_list + G160M_1577A_wave_list+G130M_1327A_wave_list
+wave_limit_list= G160M_1600A_wave_list+G130M_1291A_wave_list+ G130M_1300A_wave_list + G140L_1105A_wave_list+G130M_1096A_wave_list + G160M_1577A_wave_list+G130M_1327A_wave_list+G130M_1309A_wave_list
 
 #wave_limit_list =  [[1250,1279]] #broad lines in target
 #wave_limit_list= [[1150, 1425]]
@@ -46,8 +48,10 @@ silicon_lines= [1190, 1193, 1195, 1197, 1207, 1260, 1265, 1304, 1309] #silicon l
 
 seg_gap_dict={'G130M':{
     '1096': [1072,1104],
+    '1222':[1201,1233],
     '1291':[1268,1298],
     '1300':[1278,1306],
+    '1309':[1286,1318],
     '1096':[1075,1106],
     '1327':[1307,1335]},
              'G140L':{'1105': [1179,1180]}, 
