5628a3cca158c205e2973396b4ac48b7df12421f
Ben Kaiser <bkaiser94@gmail.com>
2019-07-12 14:36:10 -0400

    made datasets_sorting.py to do the sorting of the jumble of 235 datasets into directories in the format expected by my lightcurve and dual plotting scripts to see them.

---
+++ datasets_sorting.py
@@ -0,0 +1,96 @@
+"""
+Created by Ben Kaiser (UNC-Chapel Hill) 2019-07-11
+
+
+Should take the myriad datasets from '1retrieval/' and then put them into new directories that should follow the 
+standard naming convention. The new directories will have to be generated from the .txt file that contains all of 
+the datasets and affiliated information about the observations.
+
+
+
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import os
+from glob import glob
+from astropy.table import Table, Column
+import sys
+import csv
+
+
+
+import config
+
+
+
+info_file= 'SNAP_done.csv'
+massive_dir= '2retrieval/' #directory that contains the datasets that need to be sorted into the other directories
+targeting_file = 'targets_for_lc.txt' #filename for the collected directories to be evaluated by lazy_step_grid.py
+#all_array=np.genfromtxt(info_file, delimiter='/t', names=True).T
+#all_table= Table.read(info_file, format='ascii.csv')
+#all_array= np.genfromtxt(info_file, delimiter=',', names=True, dtype='S32')
+#print(all_array['Grating'])
+
+#############################
+replacement_list=[
+    ['+', 'p'],
+    ['-','m']
+    ]
+
+
+
+def make_dir_name(input_row):
+    wd_name = str(row['Target Name'])
+    grating= str(row['Grating'])
+    print(wd_name)
+    print('replacing characters')
+    for rep in replacement_list:
+        wd_name=wd_name.replace(rep[0], rep[1])
+        print('cleaned name:', wd_name)
+    dir_name= wd_name+'_'+grating+'/'
+    print('dir_name:', dir_name)
+    return dir_name
+
+def get_files_associated(file_association):
+    core_assoc= file_association[:6]
+    print('core_assoc', core_assoc)
+    coll_files= glob(massive_dir+core_assoc)
+    print('coll_files', coll_files)
+    return
+
+
+###############################
+
+counter=0
+collected_lists= []
+with open(info_file, 'rb') as csvfile:
+    reader= csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        if counter==0:
+            names_list= row
+        else:
+            collected_lists.append(row)
+        counter+=1
+        print(row)
+        
+print(collected_lists)
+print(len(collected_lists))
+
+info_array= np.array(collected_lists)
+print('shape', info_array.shape)
+print('len(names_list)', len(names_list))
+names_tuple= tuple(names_list)
+print('names_tuple', names_tuple)
+#info_array=info_array.T
+
+info_table= Table(info_array, names=names_tuple)
+
+info_table.pprint()
+
+for row in info_table:
+    make_dir_name(row)
+    get_files_associated(row['File'])
+
+
